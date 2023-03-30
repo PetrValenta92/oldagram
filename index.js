93645d537adb37fc8167ce36c3fc38726cf0a440
Petr Valenta <petr.valenta.92@gmail.com>
2023-03-31 00:16:19 +0200

    Update index.js
    Data are moved to data.js; functions take post data and render posts

--- index.js
+++ index.js
@@ -1,30 +1,37 @@
-const posts = [
-    {
-        name: "Vincent van Gogh",
-        username: "vincey1853",
-        location: "Zundert, Netherlands",
-        avatar: "images/avatar-vangogh.jpg",
-        post: "images/post-vangogh.jpg",
-        comment: "just took a few mushrooms lol",
-        likes: 21
-    },
-    {
-        name: "Gustave Courbet",
-        username: "gus1819",
-        location: "Ornans, France",
-        avatar: "images/avatar-courbet.jpg",
-        post: "images/post-courbet.jpg",
-        comment: "i'm feelin a bit stressed tbh",
-        likes: 4
-    },
-        {
-        name: "Joseph Ducreux",
-        username: "jd1735",
-        location: "Paris, France",
-        avatar: "images/avatar-ducreux.jpg",
-        post: "images/post-ducreux.jpg",
-        comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
-        likes: 152
-    }
-]
+import { postsData } from './data.js';
 
+function getFeedHtml(){
+    let feedHtml = ''
+
+    postsData.forEach(function(post){
+        feedHtml += `
+        <div class="container post">
+            <div class="post__profile">
+                <img class="avatar post__avatar" src="./${post.avatar}" alt="Portraite of ${post.name}">
+                <div class="post__info">
+                    <h1 class="post__name">${post.name}</h1>
+                    <h2 class="post__location">${post.location}</h2>  
+                </div>
+            </div>
+            <img class="post__img" src="./${post.post}" alt="The post with a portraite of ${post.name}">
+            <div class="post__body">
+                <div class="post__icons">
+                    <img class="icon" src="./images/icons/icon-heart.png" alt="icon of like">
+                    <img class="icon" src="./images/icons/icon-comment.png" alt="icon of comment">
+                    <img class="icon" src="./images/icons/icon-dm.png" alt="icon of direct message">
+                </div>
+                <p class="post__like-count">${post.likes} likes</p> 
+                <p class="post__caption"><span class="post__username">${post.username}</span> ${post.comment}</p>
+            </div>
+        </div>
+        `
+    });
+
+    return feedHtml;
+}
+
+function render(){
+    document.getElementById('feed').innerHTML = getFeedHtml();
+}
+
+render();
