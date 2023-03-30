998c5af73fde26182a99ad4bcb71406ebb32f53e
Petr Valenta <petr.valenta.92@gmail.com>
2023-03-31 00:14:02 +0200

    Initial commit
    index.js takes data from this file to be rendered

---
+++ data.js
@@ -0,0 +1,30 @@
+export const postsData = [
+    {
+        name: "Vincent van Gogh",
+        username: "vincey1853",
+        location: "Zundert, Netherlands",
+        avatar: "images/avatars/avatar-vangogh.jpg",
+        post: "images/posts/post-vangogh.jpg",
+        comment: "just took a few mushrooms lol",
+        likes: 21
+    },
+    {
+        name: "Gustave Courbet",
+        username: "gus1819",
+        location: "Ornans, France",
+        avatar: "images/avatars/avatar-courbet.jpg",
+        post: "images/posts/post-courbet.jpg",
+        comment: "i'm feelin a bit stressed tbh",
+        likes: 4
+    },
+        {
+        name: "Joseph Ducreux",
+        username: "jd1735",
+        location: "Paris, France",
+        avatar: "images/avatars/avatar-ducreux.jpg",
+        post: "images/posts/post-ducreux.jpg",
+        comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
+        likes: 152
+    }
+]
+
